356da08deccd75a6871a259a5850889967025721
KanyantaMk <159107489+KanyantaMk@users.noreply.github.com>
2024-02-11 15:40:47 +0200

    Registration update
    Registration update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0a81713db1fac22f0d4da81e91f94945",
-"/": "0a81713db1fac22f0d4da81e91f94945",
-"main.dart.js": "c260733873ee239f607c7d825c58dddb",
+"index.html": "f4250ff61825cb543fc0c8756dcb29e1",
+"/": "f4250ff61825cb543fc0c8756dcb29e1",
+"main.dart.js": "10a8c59d2ebb9365b4b4256afa226aea",
 "manifest.json": "7b0dcdbd30cd62dacf5d2a16dbb897fe",
 "version.json": "fd60d5c5ddf7a8f40da7f76f0759401f"};
 // The application shell files that are downloaded before a service worker can
